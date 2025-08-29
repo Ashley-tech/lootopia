812e6af1c277df5e3d1cc56b16b1b9f49e615e91
Ashley R. <ashleyrakotoarisoa@gmail.com>
2025-08-29 17:48:55 +0200

    Bouton "Supprimer le compte"

--- backlog/index.php
+++ backlog/index.php
@@ -82,6 +82,11 @@ $comptes = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                 </button>
                             <?php endif; ?>
                         </td>
+                        <td>
+                                <button onclick="window.location.href='/controllers/delete_account.php?id=<?= urlencode($compte['id']) ?>'">
+                                    Supprimer ce compte
+                                </button>
+                        </td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
